3aeb69c6667643763085debd42d7b7565242440e
ilge <ilgea@eecs.berkeley.edu>
2016-04-19 14:15:01 -0700

    gvaccessor

--- GoogleVision.js
+++ GoogleVision.js
@@ -1,112 +1,186 @@
-/* An attribute for a visible text annotation.
+// Copyright (c) 2014-2015 The Regents of the University of California.
+// All rights reserved.
 
- Copyright (c) 2004-2014 The Regents of the University of California.
- All rights reserved.
- Permission is hereby granted, without written agreement and without
- license or royalty fees, to use, copy, modify, and distribute this
- software and its documentation for any purpose, provided that the above
- copyright notice and the following two paragraphs appear in all copies
- of this software.
+// Permission is hereby granted, without written agreement and without
+// license or royalty fees, to use, copy, modify, and distribute this
+// software and its documentation for any purpose, provided that the above
+// copyright notice and the following two paragraphs appear in all copies
+// of this software.
 
- IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
- FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
- ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
- THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
- SUCH DAMAGE.
+// IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+// FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+// THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
 
- THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
- INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
- PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
- CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
- ENHANCEMENTS, OR MODIFICATIONS.
+// THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+// PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+// CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+// ENHANCEMENTS, OR MODIFICATIONS.
 
- PT_COPYRIGHT_VERSION_2
- COPYRIGHTENDKEY
-
+/** Retrieve a location given an address.
+ *  The location is given as an object with two numeric fields,
+ *  "latitude" and "longitude". For example,
+ *  `{"latitude": 37.85, "longitude": -122.26}` is
+ *  the location of Berkeley, California.
+ *
+ *  This accessor requires a "key" for the Google Geocoding API, which you can
+ *  obtain for free at https://developers.google.com/maps/documentation/geocoding/intro .
+ *
+ *  This accessor does not block waiting for the response, but if any additional
+ *  *address* input is received before a pending request has received a response
+ *  or timed out, then the new request will be queued and sent out only after
+ *  the pending request has completed. This strategy ensures that outputs are
+ *  produced in the same order as the input requests.
+ *
+ *  @accessor services/GeoCoder
+ *  @input {string} address The address, for example "Berkeley, CA".
+ *  @output location The location, as an object with a 'latitude' and 'longitude'
+ *   property.
+ *  @parameter {string} key The key for the Google geocoding API.
+ *  @output response An object containing the location information.
+ *  @author Edward A. Lee
+ *  @version $$Id: GeoCoder.js 546 2016-02-03 02:07:57Z cxh $$
  */
-package ptolemy.vergil.kernel.attributes;
 
-import ptolemy.actor.gui.style.TextStyle;
-import ptolemy.kernel.util.Attribute;
-import ptolemy.kernel.util.IllegalActionException;
-import ptolemy.kernel.util.NameDuplicationException;
-import ptolemy.kernel.util.NamedObj;
-import ptolemy.kernel.util.StringAttribute;
-import ptolemy.vergil.basic.RelativeLocatable;
+// Stop extra messages from jslint and jshint.  Note that there should
+// be no space between the / and the * and global. See
+// https://chess.eecs.berkeley.edu/ptexternal/wiki/Main/JSHint */
+/*globals addInputHandler, get, getParameter, error, exports, extend, get, input, output, parameter, require, send */
+/*jshint globalstrict: true*/
+'use strict';
 
-///////////////////////////////////////////////////////////////////
-//// TextAttribute
-
-/**
- <p>
- This is an attribute that is rendered as text annotation.
- </p>
- @author Edward A. Lee
- @version $Id: TextAttribute.java 70402 2014-10-23 00:52:20Z cxh $
- @since Ptolemy II 4.0
- @Pt.ProposedRating Yellow (eal)
- @Pt.AcceptedRating Red (cxh)
+/** Set up the accessor by defining the inputs and outputs.
  */
-public class TextAttribute extends AbstractTextAttribute implements
-RelativeLocatable {
+exports.setup = function() {
+    this.extend('net/REST');
+    this.input('image');
+    this.output('annotation');
+    this.output('emotion');
+    this.output('hueCmd',{'type':'JSON'});
+    this.parameter('key', {'type':'string', 'value':'Enter Key Here'}); // FIXME: key is now hard coded
+    
+    // Change default values of the base class inputs.
+    // Also, hide base class inputs, except trigger.
+    // Note the need for quotation marks on the options parameter.
+    this.input('options', {'visibility':'expert', 'value':{"headers":{"Content-Type":"application/json"}, "method":"POST", "url":"https://vision.googleapis.com"}});
+    this.input('command', {'visibility':'expert', 'value':'/v1/images:annotate/'});
+    // NOTE: The value can be given as a JSON string or a JavaScript object.
+    this.input('arguments', {'visibility':'expert', 'value':{"alt" : "json", "key":""}});
+    this.input('body', {'visibility':'expert'});
+    this.input('trigger', {'visibility':'expert'});
+    this.output('headers', {'visibility':'expert'});
+    this.output('status', {'visibility':'expert'});
+    this.parameter('outputCompleteResponsesOnly', {'visibility':'expert'});
+};
 
-    // FIXME: It may be possible to make a base class implement
-    // RelativeLocatable, but right now, if we do that, ShapeAttribute
-    // gets messed up because it includes in its bounding box the line
-    // that gets drawn to the relative object. So for now, we restrict
-    // the relative locatable capability to text attributes.
-
-    /** Construct an attribute with the given name contained by the
-     *  specified container. The container argument must not be null, or a
-     *  NullPointerException will be thrown.  This attribute will use the
-     *  workspace of the container for synchronization and version counts.
-     *  If the name argument is null, then the name is set to the empty
-     *  string. Increment the version of the workspace.
-     *  @param container The container.
-     *  @param name The name of this attribute.
-     *  @exception IllegalActionException If the attribute is not of an
-     *   acceptable class for the container, or if the name contains a period.
-     *  @exception NameDuplicationException If the name coincides with
-     *   an attribute already in the container.
-     */
-    public TextAttribute(NamedObj container, String name)
-            throws IllegalActionException, NameDuplicationException {
-        super(container, name);
-
-        // Don't use StringParameter here because variable
-        // substitution would be strange.
-        text = new StringAttribute(this, "text");
-        text.setExpression("Double click to edit text.");
-
-        TextStyle style = new TextStyle(text, "_style");
-        style.height.setExpression("15");
-        style.width.setExpression("40");
-    }
-
-    ///////////////////////////////////////////////////////////////////
-    ////                         parameters                        ////
-
-    /** The text.  This is a string that defaults to
-     *  "Double click to edit text."
-     */
-    public StringAttribute text;
-
-    ///////////////////////////////////////////////////////////////////
-    ////                         public methods                    ////
-
-    /** React to a changes in the attributes by changing the icon.
-     *  @param attribute The attribute that changed.
-     *  @exception IllegalActionException If the change is not acceptable
-     *   to this container (should not be thrown).
-     */
-    @Override
-    public void attributeChanged(Attribute attribute)
-            throws IllegalActionException {
-        if (attribute == text) {
-            _icon.setText(text.getExpression());
+exports.initialize = function() {
+    // Be sure to call the superclass so that the trigger input handler gets registered.
+    exports.ssuper.initialize.call(this);
+    
+    //var key = this.getParameter('key');
+    //if (key == "Enter Key Here") {
+    //    throw "GeoCoder:  You need a key, which you can obtain at https://developers.google.com/maps/documentation/geocoding/intro.";
+    //}
+    var self = this;
+    
+    // Handle location information.
+    this.addInputHandler('image', function() {
+        var image = this.get('image');
+        if (image) {
+            var body = {
+                'requests' : [  { "image" : {"content" : ""}, 
+                                  "features" : [  { "type" : "FACE_DETECTION", 
+                                                    "maxResults" : 1
+                                                  }
+                                               ]
+                                }
+                             ]
+            };
+            body.requests[0].image.content = image;
+            //console.log(JSON.stringify(body));
+            self.send('body', JSON.stringify(body));
+            self.send('trigger', true);
         } else {
-            super.attributeChanged(attribute);
+            throw 'GoogleVision: No image.';
+        }
+    });
+};
+
+/** Filter the response, extracting the latitude and longitude and
+ *  formatting.
+ */
+exports.filterResponse = function(response) {
+    if (response) {
+        // Note that for some hosts, the response is a string, needing to parsed,
+        // and for some, its already been parsed.
+        var parsed = response;
+        
+        if (typeof parsed === 'string') {
+            try {
+                parsed = JSON.parse(response);
+            } catch (err) {
+                error('GeoCoder: Unable to parse response: ' + err.message +
+                        '\nResponse was: ' + response);
+                // So that downstream actors don't just a previous location, send null.
+                //this.send('location', null);
+            }
+        }
+        
+        if (parsed && parsed.responses) { 
+           
+           //console.log(parsed.responses[0]);
+           
+            if (parsed.responses[0].faceAnnotations) {
+                
+                
+                var result = parsed.responses[0].faceAnnotations[0];
+                 
+                //console.log(result);
+                var emotionNames = ['joy','sorrow','anger','surprise','unknown'];
+                var emotions = [result.joyLikelihood, result.sorrowLikelihood,
+                result.angerLikelihood, result.surpriseLikelihood];
+                
+                var emotionLikelihood = [0,0,0,0];
+                var emotionColorMap = [25500, 46920, 65280, 12750, 0];
+                var maxSoFar = -2;
+                var currentEmotion = 'neutral';
+                var currentEmotionIndex = 4;
+                for (var i=0; i < emotions.length; i++) {
+                    switch(emotions[i]) {
+                        case 'UNKNOWN': emotionLikelihood[i]= -10; break;
+                        case 'VERY_UNLIKELY': emotionLikelihood[i] = -2; break;
+                        case 'UNLIKELY': emotionLikelihood[i] = -1; break;
+                        case 'POSSIBLE': emotionLikelihood[i] = 1; break;
+                        case 'LIKELY': emotionLikelihood[i] = 2; break;
+                        case 'VERY_LIKELY': emotionLikelihood[i] = 3; break;
+                        default: emotionLikelihood[i] = -10; break;
+                    }
+                    
+                    if (emotionLikelihood[i] > maxSoFar) {
+                        currentEmotion = emotionNames[i];
+                        currentEmotionIndex = i;
+                    }
+                }
+                console.log(emotionLikelihood);
+                this.send('emotion', currentEmotion);
+                var hueCommand =[{'id':7,'on':false},{'id':8,'on':false},{'id':9,'on':false}]; 
+                var hue = 0;
+                if ( !currentEmotion.equals('neutral')) { 
+                    hue = emotionColorMap[currentEmotionIndex];
+                    hueCommand = [{'id':7,'on':true,'hue':hue, 'sat':255},
+                    {'id':8,'on':true,'hue':hue,'sat':255},
+                    {'id':9,'on':true,'hue':hue,'sat':255}];
+                }
+               
+                 
+                
+                this.send('hueCmd', hueCommand);
+            }
+            
         }
     }
-}
+    return response;
+};
